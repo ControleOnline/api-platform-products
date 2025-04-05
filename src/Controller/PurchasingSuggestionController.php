b4d1877af6bfc64debbe23e67ee711beeb149e98
Luiz Kim <luiz.kim@controleonline.com>
2025-04-05 20:28:06 -0300

    Update

--- src/Controller/PurchasingSuggestionController.php
+++ src/Controller/PurchasingSuggestionController.php
@@ -19,7 +19,7 @@ class PurchasingSuggestionController extends AbstractController
     ) {}
 
     /**
-     * @Route("/orders/purchasing-suggestion", name="purchasing_suggestion", methods={"GET"})
+     * @Route("/products/purchasing-suggestion", name="purchasing_suggestion", methods={"GET"})
      * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_CLIENT')")
      */
     public function getPurchasingSuggestion(Request $request): JsonResponse
