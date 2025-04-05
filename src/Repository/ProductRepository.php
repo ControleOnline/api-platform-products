234fb82184521705b8153fe6800f98aaf25e3817
Luiz Kim <luiz.kim@controleonline.com>
2025-04-05 20:21:47 -0300

    Update

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -2,29 +2,23 @@
 
 namespace ControleOnline\Repository;
 
-use ControleOnline\Entity\Product;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
+use ControleOnline\Entity\Order;
 use ControleOnline\Entity\People;
 use ControleOnline\Service\PeopleService;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\DBAL\DBALException;
 use Doctrine\ORM\Query\ResultSetMapping;
 
-/**
- * @method Product|null find($id, $lockMode = null, $lockVersion = null)
- * @method Product|null findOneBy(array $criteria, array $orderBy = null)
- * @method Product[]    findAll()
- * @method Product[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
-class ProductRepository extends ServiceEntityRepository
+class OrderRepository extends ServiceEntityRepository
 {
     public function __construct(
         private PeopleService $peopleService,
+
         ManagerRegistry $registry
     ) {
-        parent::__construct($registry, Product::class);
+        parent::__construct($registry, Order::class);
     }
-
 
     public function getPurchasingSuggestion(?People $company): array
     {
@@ -40,13 +34,15 @@ class ProductRepository extends ServiceEntityRepository
                 'p.type AS type',
                 'SUM(pi.available + pi.ordered + pi.transit - pi.sales) AS stock',
                 'SUM(pi.minimum) AS minimum',
-                '(CASE WHEN SUM(pi.available + pi.ordered + pi.transit - pi.minimum - pi.sales) * -1 < 0 THEN 0 ELSE SUM(pi.available + pi.ordered + pi.transit - pi.minimum - pi.sales) * -1 END) AS needed'
+                '(CASE WHEN SUM(pi.available + pi.ordered + pi.transit - pi.minimum - pi.sales) * -1 < 0 THEN 0 ELSE SUM(pi.available + pi.ordered + pi.transit - pi.minimum - pi.sales) * -1 END) AS needed',
+                'pu.productUnit AS unity'
             ])
             ->join('p.company', 'pe')
             ->join('ControleOnline\Entity\ProductInventory', 'pi', 'WITH', 'pi.product = p.id')
+            ->leftJoin('p.unity', 'pu')
             ->andWhere('p.type NOT IN (:excludedTypes)')
             ->andWhere('pe IN (:companies)')
-            ->groupBy('p.id, p.product, p.description, p.sku, pe.id, pe.name, pe.alias')
+            ->groupBy('p.id, p.product, p.description, p.sku, pe.id, pe.name, pe.alias, pu.productUnit')
             ->having('needed > 0')
             ->setParameter('excludedTypes', ['custom', 'component'])
             ->setParameter(
