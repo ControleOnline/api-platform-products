c57b488ba2672208ff794d8715dac2b93740d5e2
Luiz Kim <luiz.kim@controleonline.com>
2025-04-06 00:16:00 -0300

    Update

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -7,8 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use ControleOnline\Entity\People;
 use ControleOnline\Service\PeopleService;
-use Doctrine\DBAL\DBALException;
-use Doctrine\ORM\Query\ResultSetMapping;
+use Doctrine\ORM\QueryBuilder;
 
 /**
  * @method Product|null find($id, $lockMode = null, $lockVersion = null)
@@ -25,9 +24,80 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
+    private function getInventoryData(): QueryBuilder
+    {
+        return $this->createQueryBuilder('p')
+            ->select([
+                'op.inventory AS inventory',
+                'op.product AS product',
+                "SUM(CASE WHEN o.orderType = 'purchasing' AND o.status IN (:purchasing_status) THEN op.quantity ELSE 0 END) - 
+                 SUM(CASE WHEN o.orderType = 'sale' AND o.status IN (:sales_status) THEN op.quantity ELSE 0 END) AS available",
+                "SUM(CASE WHEN o.orderType = 'purchasing' AND o.status IN (:ordered_status) THEN op.quantity ELSE 0 END) AS ordered",
+                "SUM(CASE WHEN o.orderType = 'purchasing' AND o.status IN (:transit_status) THEN op.quantity ELSE 0 END) AS transit",
+                '0 AS minimum',
+                '0 AS maximum',
+                "SUM(CASE WHEN o.orderType = 'sale' AND o.status IN (:sales_status) THEN op.quantity ELSE 0 END) AS sales"
+            ])
+            ->from('ControleOnline\Entity\Order', 'o')
+            ->join('o.orderProducts', 'op')
+            ->join('op.product', 'p')
+            ->andWhere("o.orderType IN ('purchasing', 'sale')")
+            ->andWhere('o.status IN (:all_status)')
+            ->andWhere("p.type IN ('product', 'feedstock')")
+            ->andWhere(
+                "(o.orderType = 'sale' AND o.provider IN (:provider_id)) OR 
+                 (o.orderType = 'purchasing' AND o.client IN (:client_id))"
+            )
+            ->groupBy('op.inventory, op.product');
+    }
+
+    public function updateInventory(): void
+    {
+        $em = $this->getEntityManager();
+
+        $purchasingStatus = [7];
+        $orderedStatus = [5];
+        $transitStatus = [6];
+        $salesStatus = [6];
+        $allStatus = array_unique(array_merge($purchasingStatus, $orderedStatus, $transitStatus, $salesStatus));
+
+        try {
+            $em->getConnection()->beginTransaction();
+
+            $inventoryQb = $this->getInventoryData();
+
+            $qb = $this->createQueryBuilder('p')
+                ->update('ControleOnline\Entity\ProductInventory', 'pi')
+                ->set('pi.available', 'inv.available')
+                ->set('pi.ordered', 'inv.ordered')
+                ->set('pi.transit', 'inv.transit')
+                ->set('pi.minimum', 'inv.minimum')
+                ->set('pi.maximum', 'inv.maximum')
+                ->set('pi.sales', 'inv.sales')
+                ->from('(' . $inventoryQb->getQuery()->getDQL() . ')', 'inv')
+                ->where('pi.inventory = inv.inventory')
+                ->andWhere('pi.product = inv.product')
+                ->setParameters([
+                    'purchasing_status' => $purchasingStatus,
+                    'sales_status' => $salesStatus,
+                    'ordered_status' => $orderedStatus,
+                    'transit_status' => $transitStatus,
+                    'all_status' => $allStatus,
+                    'provider_id' =>  $this->peopleService->getMyCompanies(),
+                    'client_id' => $this->peopleService->getMyCompanies(),
+                ]);
+
+            $qb->getQuery()->execute();
+            $em->getConnection()->commit();
+        } catch (\Exception $e) {
+            $em->getConnection()->rollBack();
+            throw new \Exception($e->getMessage());
+        }
+    }
 
     public function getPurchasingSuggestion(?People $company): array
     {
+        $this->updateInventory();
         $qb = $this->createQueryBuilder('p')
             ->select([
                 'pe.id AS company_id',
