f211300f18d3ef947dcca56f01a5c6f63f474eb1
Luiz Kim <luiz.kim@controleonline.com>
2026-01-29 18:40:49 -0300

    Refactoring

--- src/Service/ProductGroupService.php
+++ src/Service/ProductGroupService.php
@@ -3,9 +3,11 @@
 namespace ControleOnline\Service;
 
 use ControleOnline\Entity\Invoice;
+use ControleOnline\Entity\Product;
+use ControleOnline\Entity\ProductGroup;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface
- as Security;
+as Security;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -13,7 +15,7 @@ class ProductGroupService
 {
     private $request;
     public function __construct(
-        private EntityManagerInterface $manager,
+        private EntityManagerInterface $entityManager,
         private Security $security,
         private PeopleService $PeopleService,
         RequestStack $requestStack
@@ -22,7 +24,29 @@ class ProductGroupService
         $this->request  = $requestStack->getCurrentRequest();
     }
 
+    public function discoveryProductGroup(Product $parentProduct, string $groupName): ProductGroup
+    {
+        $productGroup = $this->entityManager->getRepository(ProductGroup::class)->findOneBy([
+            'productGroup' => $groupName,
+            'parentProduct' => $parentProduct
+        ]);
 
+        if (!$productGroup) {
+            $productGroup = new ProductGroup();
+            $productGroup->setParentProduct($parentProduct);
+            $productGroup->setProductGroup($groupName);
+            $productGroup->setPriceCalculation('sum');
+            $productGroup->setRequired(false);
+            $productGroup->setMinimum(0);
+            $productGroup->setMaximum(0);
+            $productGroup->setActive(true);
+            $productGroup->setGroupOrder(0);
+            $this->entityManager->persist($productGroup);
+            $this->entityManager->flush();
+        }
+
+        return $productGroup;
+    }
 
     public function securityFilter(QueryBuilder $queryBuilder, $resourceClass = null, $applyTo = null, $rootAlias = null): void
     {
