da1f151c97ef2e2cc84603321a4c2740242c9334
Luiz Kim <luiz.kim@controleonline.com>
2026-03-29 19:24:27 -0300

    Inventory

--- src/Entity/ProductInventory.php
+++ src/Entity/ProductInventory.php
@@ -3,7 +3,6 @@
 namespace ControleOnline\Entity;
 
 use Symfony\Component\Serializer\Attribute\Groups;
-
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
@@ -13,16 +12,21 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Put;
-
 use ControleOnline\Repository\ProductInventoryRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ApiResource(
     operations: [
         new Get(security: 'is_granted(\'PUBLIC_ACCESS\')'),
-        new Put(security: 'is_granted(\'ROLE_CLIENT\')', denormalizationContext: ['groups' => ['product_inventory:write']]),
+        new Put(
+            security: 'is_granted(\'ROLE_CLIENT\')',
+            denormalizationContext: ['groups' => ['product_inventory:write']]
+        ),
         new Delete(security: 'is_granted(\'ROLE_CLIENT\')'),
-        new Post(securityPostDenormalize: 'is_granted(\'ROLE_CLIENT\')'),
+        new Post(
+            securityPostDenormalize: 'is_granted(\'ROLE_CLIENT\')',
+            denormalizationContext: ['groups' => ['product_inventory:write']]
+        ),
         new GetCollection(security: 'is_granted(\'PUBLIC_ACCESS\')'),
     ],
     formats: ['jsonld', 'json', 'html', 'jsonhal', 'csv' => ['text/csv']],
@@ -35,64 +39,58 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Index(name: 'inventory_id', columns: ['inventory_id'])]
 #[ORM\Index(name: 'product_id', columns: ['product_id'])]
 #[ORM\Entity(repositoryClass: ProductInventoryRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class ProductInventory
 {
-    #[ORM\Column(name: 'id', type: 'integer', nullable: false)]
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: 'IDENTITY')]
-    #[Groups(['product_inventory:read', 'product_inventory:write'])]
+    #[ORM\Column(type: 'integer')]
+    #[Groups(['product_inventory:read'])]
     private $id;
 
-    #[ORM\JoinColumn(name: 'inventory_id', referencedColumnName: 'id', nullable: false)]
     #[ORM\ManyToOne(targetEntity: Inventory::class)]
+    #[ORM\JoinColumn(nullable: false)]
     #[Groups(['product_inventory:read', 'product_inventory:write'])]
     private $inventory;
 
-    #[ORM\JoinColumn(name: 'product_id', referencedColumnName: 'id', nullable: false)]
     #[ORM\ManyToOne(targetEntity: Product::class)]
+    #[ORM\JoinColumn(nullable: false)]
     #[Groups(['product_inventory:read', 'product_inventory:write'])]
     private $product;
 
-    #[ORM\Column(name: 'available', type: 'integer', nullable: false, options: ['default' => 0])]
-    #[Groups(['product_inventory:read', 'product_inventory:write'])]
-    private $available = 0;
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    #[Groups(['product_inventory:read'])]
+    private int $available = 0;
 
-    #[ORM\Column(name: 'sales', type: 'integer', nullable: false, options: ['default' => 0])]
-    #[Groups(['product_inventory:read', 'product_inventory:write'])]
-    private $sales = 0;
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    #[Groups(['product_inventory:read'])]
+    private int $sales = 0;
 
-    #[ORM\Column(name: 'ordered', type: 'integer', nullable: false, options: ['default' => 0])]
-    #[Groups(['product_inventory:read', 'product_inventory:write'])]
-    private $ordered = 0;
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    #[Groups(['product_inventory:read'])]
+    private int $ordered = 0;
 
-    #[ORM\Column(name: 'transit', type: 'integer', nullable: false, options: ['default' => 0])]
-    #[Groups(['product_inventory:read', 'product_inventory:write'])]
-    private $transit = 0;
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
+    #[Groups(['product_inventory:read'])]
+    private int $transit = 0;
 
-    #[ORM\Column(name: 'minimum', type: 'integer', nullable: false, options: ['default' => 0])]
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
     #[Groups(['product_inventory:read', 'product_inventory:write'])]
-    private $minimum = 0;
+    private int $minimum = 0;
 
-    #[ORM\Column(name: 'maximum', type: 'integer', nullable: false, options: ['default' => 0])]
+    #[ORM\Column(type: 'integer', options: ['default' => 0])]
     #[Groups(['product_inventory:read', 'product_inventory:write'])]
-    private $maximum = 0;
+    private int $maximum = 0;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function setId(int $id): self
-    {
-        $this->id = $id;
-        return $this;
-    }
-
     public function getInventory(): ?Inventory
     {
         return $this->inventory;
     }
-
     public function setInventory(Inventory $inventory): self
     {
         $this->inventory = $inventory;
@@ -103,7 +101,6 @@ class ProductInventory
     {
         return $this->product;
     }
-
     public function setProduct(Product $product): self
     {
         $this->product = $product;
@@ -114,51 +111,40 @@ class ProductInventory
     {
         return $this->available;
     }
-
-    public function setAvailable(int $available): self
-    {
-        $this->available = $available;
-        return $this;
-    }
-
     public function getSales(): int
     {
         return $this->sales;
     }
-
-    public function setSales(int $sales): self
-    {
-        $this->sales = $sales;
-        return $this;
-    }
-
     public function getOrdered(): int
     {
         return $this->ordered;
     }
-
-    public function setOrdered(int $ordered): self
-    {
-        $this->ordered = $ordered;
-        return $this;
-    }
-
     public function getTransit(): int
     {
         return $this->transit;
     }
 
-    public function setTransit(int $transit): self
+    public function setAvailable(int $v): self
     {
-        $this->transit = $transit;
-        return $this;
+        throw new \RuntimeException('available não pode ser alterado manualmente.');
+    }
+    public function setSales(int $v): self
+    {
+        throw new \RuntimeException('sales não pode ser alterado manualmente.');
+    }
+    public function setOrdered(int $v): self
+    {
+        throw new \RuntimeException('ordered não pode ser alterado manualmente.');
+    }
+    public function setTransit(int $v): self
+    {
+        throw new \RuntimeException('transit não pode ser alterado manualmente.');
     }
 
     public function getMinimum(): int
     {
         return $this->minimum;
     }
-
     public function setMinimum(int $minimum): self
     {
         $this->minimum = $minimum;
@@ -169,7 +155,6 @@ class ProductInventory
     {
         return $this->maximum;
     }
-
     public function setMaximum(int $maximum): self
     {
         $this->maximum = $maximum;
